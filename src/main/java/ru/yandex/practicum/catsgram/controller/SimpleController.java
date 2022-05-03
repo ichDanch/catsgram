98ea7741d7239ccbdeef5e9f6942be429dc263a7
Danila <somevildhjarta@gmail.com>
2022-05-03 20:18:42 +0300

    catsgram

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "Котограм";
+    }
+}
